a88ab12fdb4b0805f1b869c4db40e98ebbf2df21
meekhumor <om17092004@gmail.com>
2025-01-04 15:17:52 +0530

    Refactor Header component to import Link as a named import from react-router-dom

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Link from "react-router-dom"
+import {Link} from "react-router-dom"
 const Header = () => {
   return (
     <header className="bg-purple1 text-white p-4 shadow-md">
